9134dcb3c771115580823261662e8a53407ba438
Michelon Souza <michelon.souza@hotmail.com>
2020-04-08 23:30:28 -0300

    feature: add loadRepositories and funcionality to handleLikeRepository

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,76 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api';
+
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  async function loadRepositories() {
+    const { data } = await api.get('/repositories');
+
+    setRepositories(data);
+  }
+
+  useEffect(() => {
+    loadRepositories();
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    await api.post(`/repositories/${id}/like`);
+
+    const newRepositories = repositories.map(repo => 
+      repo.id === id 
+      ? {...repo, likes: repo.likes += 1}
+      : repo
+    );
+
+    setRepositories(newRepositories);
+  }
+
+  function RenderRepository({item: repository}) {
+    return (
+      <View style={styles.repositoryContainer}>
+        <Text style={styles.repository}>{repository.title}</Text>
+
+        <View style={styles.techsContainer}>
+          {repository.techs.map(tech => (
+            <Text key={`${tech}-${Date.now()}`} style={styles.tech}>
+              {tech}
+            </Text>
+          ))}
+        </View>
+
+        <View style={styles.likesContainer}>
+          <Text
+            style={styles.likeText}
+            // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+            testID={`repository-likes-${repository.id}`}
+          >
+            {repository.likes} curtida{repository.likes > 1 && 's'}
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleLikeRepository(repository.id)}
+          // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+          testID={`like-button-${repository.id}`}
+        >
+          <Text style={styles.buttonText}>Curtir</Text>
+        </TouchableOpacity>
+      </View>
+    )
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={RenderRepository} />
       </SafeAreaView>
     </>
   );
@@ -65,6 +95,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     backgroundColor: "#fff",
     padding: 20,
+    borderRadius: 4,
   },
   repository: {
     fontSize: 32,
@@ -82,6 +113,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
+    borderRadius: 4,
   },
   likesContainer: {
     marginTop: 15,
@@ -95,6 +127,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+    borderRadius: 4,
   },
   buttonText: {
     fontSize: 14,
